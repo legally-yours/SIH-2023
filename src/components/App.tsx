c8855378833872fbb49a6b5c628afee4903289d5
poseidon0z <adityaprabhu1290@gmail.com>
2023-09-21 23:16:40 +0530

    Added a homepage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,24 +4,26 @@ import InfoCard from "./InfoCard";
 import { Typography } from "@mui/material";
 import EmptyImage from "../assets/EmptyImage.png";
 import Calendar from './Calender.tsx';
+import HomePage from "./HomePage.tsx";
 //import "./App.css";
 
 function App() {
   const currentDate = new Date();
-  return (
-    <>
-      <Navbar></Navbar>
-      <InfoCard header="Rohit Sinha" image={EmptyImage}>
-        <Typography variant="body2" color="#0077B6">
-          Expert In Civilian And Financial Cases.
-        </Typography>
-      </InfoCard>
-      <div className="Calendar">
-      <Calendar date={currentDate} />
-    </div>
+  // return (
+  //   <>
+  //     <Navbar></Navbar>
+  //     <InfoCard header="Rohit Sinha" image={EmptyImage}>
+  //       <Typography variant="body2" color="#0077B6">
+  //         Expert In Civilian And Financial Cases.
+  //       </Typography>
+  //     </InfoCard>
+  //     <div className="Calendar">
+  //     <Calendar date={currentDate} />
+  //   </div>
 
-    </>
-  );
+  //   </>
+  // );
+  return <HomePage></HomePage>
 }
 
 export default App
