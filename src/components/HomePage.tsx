4022dcf9b402268fdef4ee2ec7a2be40f96af64e
poseidon0z <adityaprabhu1290@gmail.com>
2023-09-23 04:50:43 +0530

    ui changes and flow improvement

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,12 +2,17 @@
 import IconDescription from './IconDescription.tsx';
 import Description from './Description.tsx';
 import SummaryShortcuts from './SummaryShortcuts.tsx';
+import OnboardPanel from './OnboardPanel.tsx';
 // import Rohan from './TransalateButton.tsx';
 
-const HomePage = () => {
+interface Props {
+  setCurrentPage : React.Dispatch<React.SetStateAction<string>>;
+}
+const HomePage = ({setCurrentPage} : Props) => {
     return (
         <>
 <IconDescription></IconDescription>
+<OnboardPanel setCurrentPage={setCurrentPage}></OnboardPanel>
         <SummaryShortcuts />
         <Description></Description>
         </>
