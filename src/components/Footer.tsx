2a985c31423d6d944bf3aa791b02a272234816f3
poseidon0z <adityaprabhu1290@gmail.com>
2023-09-23 10:05:46 +0530

    Added images for people, and other features

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,15 @@
 // import React from 'react'
 import logoblack from '../assets/logo-black.png';
 import '../styles/Footer.css'
+import TransalateButton from './TransalateButton';
 
 const Footer = () => {
   return (
     <>
     <footer>
         <div className="left-footer"><div className="left-footer-element"><h3>Company</h3><p>FAQs</p><p>About us</p><p>Contact us</p></div>
-        <div className="left-footer-element"><h3>Services</h3><p>Home</p><p>Find lawyers</p></div></div>
+        <div className="left-footer-element"><h3>Services</h3><p>Home</p><p>Find lawyers</p></div>
+        <div className="left-footer-element"><TransalateButton></TransalateButton></div></div>
         <img className="right-footer" src={logoblack}></img>
     </footer>
     </>
